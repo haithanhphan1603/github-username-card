e4a306f26bbcf33a63ee73bc6d11c55bf5c62ee3
hai-phan-163 <haithanhphan163@gmail.com>
2021-10-24 19:06:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import React from "react";
+import axios from "axios";
+import "../src/App.css";
+const CardList = (props) => (
+  <div>
+    {props.profiles.map((profile) => (
+      <Card key={profile.id} {...profile} />
+    ))}
+  </div>
+);
 
-import "./App.css";
-import CardList from "./Components/CardList";
-import Form from "./Components/Form";
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  state = { userName: "" };
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await axios.get(
+      `https://api.github.com/users/${this.state.userName}`
+    );
+    this.props.onSubmit(resp.data);
+    this.setState({ userName: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder="GitHub username"
+          required
+        />
+        <button>Add card</button>
+      </form>
+    );
+  }
+}
+
 class App extends React.Component {
   state = {
     profiles: [],
